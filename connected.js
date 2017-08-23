590cfe1c87c6ce3550a2ed858839ed330ee3f84e
faridho <=>
2017-08-23 23:22:36 +0700

    Read Complete

--- connected.js
+++ connected.js
@@ -2,10 +2,10 @@
  * Created by pc on 8/23/2017.
  */
 var mysql = require('mysql');
-var con   = mysql.createConnected({
+var con   = mysql.createConnection({
    host : "localhost",
     user: "root",
     password: "",
-    database: "db_movies";
+    database: "db_movies"
 });
 module.exports = con;
